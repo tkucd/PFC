dde427cb94c3952760f86c90f1fa05f310c61559
tomo823 <121341723+tomo823@users.noreply.github.com>
2023-07-01 08:10:18 +0000

    modified:   backend/application.py

--- backend/application.py
+++ backend/application.py
@@ -1,25 +1,24 @@
 from flask import Flask
-from flask import request, make_response, jsonify
+from flask import request
+from menu import get_menu
 
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def index():
     return "text parser:)"
 
 
-@app.route("/wakati", methods=['GET','POST'])
-def parse():
+@app.route("/", methods=['POST'])
+def get_items():
     #print(request.get_json()) # -> {'post_text': 'テストテストテスト'}
     data = request.get_json()
-    text = data['post_text']
+    text = data['ingredients']
+    return get_menu(text)
 
-
-    response = {'result': res}
-    #print(response)
-    return make_response(jsonify(response))
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0:3000', port=3000)
